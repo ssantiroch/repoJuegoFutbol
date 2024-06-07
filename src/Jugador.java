60f30485dc13b3bf692088a787c6e13f423ff1bf
FaustoGimenezLobasso <126639115+FaustoGimenezLobasso@users.noreply.github.com>
2024-06-07 10:47:49 -0300

    Update Jugador.java

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,13 +1,79 @@
 public class Jugador {
 
     private String nombreJugador;
-    private Enum posicionJugador;
+    private Posiciones posicionJugador;
 
     private float costoJugador;
     private double ataqueJuegador;
     private double defensaJugador;
     private int golesJugador;
+    
+    enum Posiciones {
+        Portero,
+        Defensor,
+        Mediocampista,
+        Delantero,
+    }
 
     public Jugador() {
+    }
+
+    public String getNombreJugador() {
+        return nombreJugador;
+    }
+
+    public void setNombreJugador(String nombreJugador) {
+        this.nombreJugador = nombreJugador;
+    }
+
+    public Enum getPosicionJugador() {
+        return posicionJugador;
+    }
+
+    public void setPosicionJugador(Posiciones posicionJugador) {
+        this.posicionJugador = posicionJugador;
+    }
+
+    public float getCostoJugador() {
+        return costoJugador;
+    }
+
+    public void setCostoJugador(float costoJugador) {
+        this.costoJugador = costoJugador;
+    }
+
+    public double getAtaqueJuegador() {
+        return ataqueJuegador;
+    }
+
+    public void setAtaqueJuegador(double ataqueJuegador) {
+        this.ataqueJuegador = ataqueJuegador;
+    }
+
+    public double getDefensaJugador() {
+        return defensaJugador;
+    }
+
+    public void setDefensaJugador(double defensaJugador) {
+        this.defensaJugador = defensaJugador;
+    }
+
+    public int getGolesJugador() {
+        return golesJugador;
+    }
+
+    public void setGolesJugador(int golesJugador) {
+        this.golesJugador = golesJugador;
+    }
+    @Override
+    public String toString() {
+        return "Jugador{" +
+                "nombreJugador='" + nombreJugador + '\'' +
+                ", posicionJugador=" + posicionJugador +
+                ", costoJugador=" + costoJugador +
+                ", ataqueJuegador=" + ataqueJuegador +
+                ", defensaJugador=" + defensaJugador +
+                ", valoracionTotal=" + valoracionTotal +
+                '}';
     }
 }
