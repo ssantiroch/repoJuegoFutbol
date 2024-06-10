2857c15e0c6d45b023e667d4264f6b4d9cebb7a8
QuintoIsacch <qisacch@gmail.com>
2024-06-10 11:48:01 -0300

    Update Persona.java

--- src/Persona.java
+++ src/Persona.java
@@ -7,4 +7,47 @@ public class Persona {
 
     public Persona() {
     }
+public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(String idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getContraseñaUsuario() {
+        return contraseñaUsuario;
+    }
+
+    public void setContraseñaUsuario(String contraseñaUsuario) {
+        this.contraseñaUsuario = contraseñaUsuario;
+    }
+
+    public boolean isAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        this.administrador = administrador;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 }
+
+//FALTA: validacion de casi todos los atributos, comprobar repetidos de NombreUsuario, ID, Email
+//Buscar la manera de determinar si alguien puede ser admin o usuario}
