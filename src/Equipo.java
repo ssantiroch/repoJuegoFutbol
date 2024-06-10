e8551e9771d7ea5bbe548bf01b08507f06c3b1af
FaustoGimenezLobasso <126639115+FaustoGimenezLobasso@users.noreply.github.com>
2024-06-10 11:54:15 -0300

    Actualización Equipo.java

--- src/Equipo.java
+++ src/Equipo.java
@@ -6,12 +6,87 @@ public class Equipo {
 
     private float presupuestoEquipo;
 
-    public float puntajeEquipo;
+    public int puntajeEquipo;
 
-    private Jugador[] juegadoresEquipo;
+    private Jugador[] jugadoresEquipo;
 
     private boolean ventajaDeEquipo;
+    private int victorias;
+    private int empates;
+    private int derrotas;
 
     public Equipo() {
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public String getDtEquipo() {
+        return dtEquipo;
+    }
+
+    public void setDtEquipo(String dtEquipo) {
+        this.dtEquipo = dtEquipo;
+    }
+
+    public float getPresupuestoEquipo() {
+        return presupuestoEquipo;
+    }
+
+    public void setPresupuestoEquipo(float presupuestoEquipo) {
+        this.presupuestoEquipo = presupuestoEquipo;
+    }
+
+    public int getPuntajeEquipo() {
+        return puntajeEquipo;
+    }
+
+    public void setPuntajeEquipo(int puntajeEquipo) {
+        this.puntajeEquipo = puntajeEquipo;
+    }
+
+    public Jugador[] getJuegadoresEquipo() {
+        return jugadoresEquipo;
+    }
+
+    public void setJuegadoresEquipo(Jugador[] juegadoresEquipo) {
+        this.jugadoresEquipo = juegadoresEquipo;
+    }
+
+    public boolean isVentajaDeEquipo() {
+        return ventajaDeEquipo;
+    }
+
+    public void setVentajaDeEquipo(boolean ventajaDeEquipo) {
+        this.ventajaDeEquipo = ventajaDeEquipo;
+    }
+
+    public int getVictorias() {
+        return victorias;
+    }
+
+    public void setVictorias(int victorias) {
+        this.victorias = victorias;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
     }
 }
