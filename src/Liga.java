2d2659cc02529624e2139137cf444d1155ecbbbc
FaustoGimenezLobasso <126639115+FaustoGimenezLobasso@users.noreply.github.com>
2024-06-10 11:53:11 -0300

    Update Liga.java

--- src/Liga.java
+++ src/Liga.java
@@ -1,8 +1,17 @@
 public class Liga {
 
-    public String noombreLiga;
+    public String nombreLiga;
     public Equipo[] equiposEnLiga = new Equipo[32];
 
-    public Liga() {
+    public Liga(String nombreLiga){
+        this.nombreLiga = nombreLiga;
+    }
+
+    public String getNombreLiga() {
+        return nombreLiga;
+    }
+
+    public void setNombreLiga(String nombreLiga) {
+        this.nombreLiga = nombreLiga;
     }
 }
