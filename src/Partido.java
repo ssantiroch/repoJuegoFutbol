80527c69f70d1b95423fa2e8fbcf6da21eef8c28
FaustoGimenezLobasso <126639115+FaustoGimenezLobasso@users.noreply.github.com>
2024-06-17 16:11:44 -0300

    Update Partido.java

--- src/Partido.java
+++ src/Partido.java
@@ -1,90 +1,121 @@
-import java.util.Optional;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Partido{
-
+public class Partido {
     public Equipo equipoLocalPartido;
-    public int golesLocal;
+    private int golesLocal;
     public Equipo equipoVisitantePartido;
-    public int golesVisitante;
+    private int golesVisitante;
+    private Equipo equipoUsuario;
 
-    public Partido(Equipo equipoLocalPartido, Equipo equipoVisitantePartido) {
+    public Partido(Equipo equipoLocalPartido, Equipo equipoVisitantePartido, Equipo equipoUsuario) {
         this.equipoLocalPartido = equipoLocalPartido;
         this.equipoVisitantePartido = equipoVisitantePartido;
+        this.equipoUsuario = equipoUsuario;
     }
 
-    public int getGolesLocal() {
-        return golesLocal;
+    public void jugarPartido() {
+        if (equipoLocalPartido.equals(equipoUsuario) || equipoVisitantePartido.equals(equipoUsuario)) {
+            jugarPartidoConUsuario();
+        } else {
+            jugarPartidoSimulado();
+        }
     }
 
-    public void setGolesLocal(int golesLocal) {
-        this.golesLocal = golesLocal;
+    private void jugarPartidoSimulado() {
+        Random rand = new Random();
+        this.golesLocal = rand.nextInt(5); // Máximo de 4 goles por partido
+        this.golesVisitante = rand.nextInt(5);
+
+        // Mostrar resultados del partido
+        System.out.println("Resultado del partido simulado: ");
+        System.out.println(equipoLocalPartido.nombreEquipo + " " + golesLocal + " - " + golesVisitante + " " + equipoVisitantePartido.nombreEquipo);
+
+        // Actualizar puntos
+        actualizarPuntos();
     }
 
-    public int getGolesVisitante() {
-        return golesVisitante;
-    }
-
-    public void setGolesVisitante(int golesVisitante) {
-        this.golesVisitante = golesVisitante;
-    }
-
-    public void jugarPartido(){
+    private void jugarPartidoConUsuario() {
         Random rand = new Random();
         Scanner teclado = new Scanner(System.in);
-
-        int iL = rand.nextInt(3) + 1;
-        int iV = rand.nextInt(3)+ 1;
 
         int oportunidadesLocal = 5;
         int oportunidadesVisitante = 5;
 
-        if(equipoLocalPartido.puntajeEquipo > equipoVisitantePartido.puntajeEquipo){
+        this.golesLocal = 0;
+        this.golesVisitante = 0;
+        if (equipoLocalPartido.puntajeEquipo > equipoVisitantePartido.puntajeEquipo) {
             equipoLocalPartido.setVentajaDeEquipo(true);
             equipoVisitantePartido.setVentajaDeEquipo(false);
-        }
-        else{
+        } else {
             equipoVisitantePartido.setVentajaDeEquipo(true);
             equipoLocalPartido.setVentajaDeEquipo(false);
         }
-        while(oportunidadesLocal > 0 && oportunidadesVisitante > 0){
-            System.out.println("                Elige donde atajar                ");
-            System.out.println("   Izquierda(1) -     Centro(2)    - Derecha(3)");
+        if(equipoLocalPartido.isVentajaDeEquipo()){
+            oportunidadesVisitante = 3;
+        }else{
+            oportunidadesLocal = 3;
+        }
+        System.out.println("   Comienza el partido entre "+equipoLocalPartido.getNombreEquipo()+"-"+equipoVisitantePartido.getNombreEquipo());
+        while (oportunidadesLocal > 0 && oportunidadesVisitante > 0) {
             int opcion = teclado.nextInt();
-            System.out.println(equipoLocalPartido.nombreEquipo+" " +golesLocal + " "+golesVisitante+" " +equipoVisitantePartido.nombreEquipo);
-            if(opcion != iV){
-                System.out.println("¡¡GOL DE " +equipoLocalPartido.nombreEquipo+ "!!");
-                golesLocal++;
-                System.out.println(" "+iV);
+            if (oportunidadesLocal > 0) {
+                System.out.println(equipoLocalPartido.nombreEquipo + " " + golesLocal + " " + golesVisitante + " " + equipoVisitantePartido.nombreEquipo);
+                System.out.println("                Elige donde patear                ");
+                System.out.println("   Izquierda(1) -     Centro(2)    - Derecha(3)");
+                int iV = rand.nextInt(3) + 1;
+                System.out.println(equipoLocalPartido.nombreEquipo + " " + golesLocal + " " + golesVisitante + " " + equipoVisitantePartido.nombreEquipo);
+                if (opcion != iV) {
+                    System.out.println("¡¡GOL DE " + equipoLocalPartido.nombreEquipo + "!!");
+                    golesLocal++;
+                } else {
+                    System.out.println("Atajo el arquero de " + equipoVisitantePartido.nombreEquipo);
+                }
+            }else if(oportunidadesVisitante>0){
+                System.out.println(equipoLocalPartido.getNombreEquipo()+" "+golesLocal+"-"+golesVisitante+equipoVisitantePartido.getNombreEquipo());
+                System.out.println("                Elige donde atajar                ");
+                System.out.println("   Izquierda(1) -     Centro(2)    - Derecha(3)");
+                opcion = teclado.nextInt();
+                int iL = rand.nextInt(3) + 1;
+                if (opcion == iL) {
+                    System.out.println("¡¡Atajaste!!");
+                } else {
+                    System.out.println("¡¡Gol de " + equipoVisitantePartido.nombreEquipo + "!!");
+                    golesVisitante++;
+                }
+                System.out.println(equipoLocalPartido.nombreEquipo + " " + golesLocal + " " + golesVisitante + " " + equipoVisitantePartido.nombreEquipo);
             }
-            else{
-                System.out.println("Atajo el arquero de "+ equipoVisitantePartido.nombreEquipo);
-            }
-            System.out.println("                Elige donde atajar                ");
-            System.out.println("   Izquierda(1) -     Centro(2)    - Derecha(3)");
-            opcion = teclado.nextInt();
-            if(opcion == iL){
-                System.out.println("¡¡Atajaste!!");
-                System.out.println(" "+iL);
-            }
-            else{
-                System.out.println("¡¡Gol de "+equipoVisitantePartido.nombreEquipo+"!!");
-            }
+
             oportunidadesLocal--;
             oportunidadesVisitante--;
         }
-        if(golesLocal > golesVisitante){
-            int victoria = equipoLocalPartido.getVictorias();
-            equipoLocalPartido.setVictorias(victoria++);
-            int derrota = equipoVisitantePartido.getDerrotas();
-            equipoVisitantePartido.setDerrotas(derrota++);
+
+        System.out.println("Resultado del partido: ");
+        System.out.println(equipoLocalPartido.nombreEquipo + " " + golesLocal + " - " + golesVisitante + " " + equipoVisitantePartido.nombreEquipo);
+
+        // Actualizar puntos
+        actualizarPuntos();
+    }
+
+    private void actualizarPuntos() {
+        if (golesLocal > golesVisitante) {
+            equipoLocalPartido.sumarPuntos(3);
+            equipoVisitantePartido.setDerrotas(+1);
+            equipoLocalPartido.setVictorias(+1);
+            equipoLocalPartido.sumarPresupuesto(2000000);
+        } else if (golesLocal < golesVisitante) {
+            equipoVisitantePartido.sumarPuntos(3);
+            equipoLocalPartido.setDerrotas(+1);
+            equipoVisitantePartido.setVictorias(+1);
+            equipoLocalPartido.sumarPresupuesto(500000);
+        } else {
+            equipoLocalPartido.sumarPuntos(1);
+            equipoVisitantePartido.sumarPuntos(1);
+            equipoLocalPartido.setEmpates(+1);
+            equipoVisitantePartido.setEmpates(+1);
+            equipoLocalPartido.sumarPresupuesto(1000000);
         }
-        else{
-            int victoria = equipoVisitantePartido.getVictorias();
-            equipoVisitantePartido.setVictorias(victoria++);
-            int derrota = equipoLocalPartido.getDerrotas();
-            equipoLocalPartido.setDerrotas(derrota++);
-        }
+        equipoLocalPartido.setVentajaDeEquipo(false);
+        equipoVisitantePartido.setVentajaDeEquipo(false);
     }
 }
