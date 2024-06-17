2b39f68ccb9afe5d4852124b5acfecb54515c221
FaustoGimenezLobasso <126639115+FaustoGimenezLobasso@users.noreply.github.com>
2024-06-17 16:10:08 -0300

    Update Liga.java

--- src/Liga.java
+++ src/Liga.java
@@ -1,53 +1,107 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
+import java.util.Scanner;
 
 public class Liga {
     private List<Equipo> equipos;
+    private Equipo equipoUsuario;
+    private List<Partido> partidos;
+    private int partidoActual;
 
-    public Liga(List<Equipo> equipos) {
+    public Liga(List<Equipo> equipos, Equipo equipoUsuario) {
         this.equipos = equipos;
+        this.equipoUsuario = equipoUsuario;
+        this.partidos = new ArrayList<>();
+        this.partidoActual = 0;
+        generarCalendario();
     }
 
-    public void jugarPartidoConUsuario() {
-        Equipo equipoUsuario = null;
-        for (Equipo equipo : equipos) {
-            if (equipo.nombreEquipo.equals("Equipo del Usuario")) {
-                equipoUsuario = equipo;
-                break;
+    public void mostrarMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
+        while (opcion != 5) {
+            System.out.println("\nMenú de Liga:");
+            System.out.println("1. Jugar partido");
+            System.out.println("2. Ver tabla de posiciones");
+            System.out.println("3. Ver calendario");
+            System.out.println("4. Modificar equipo");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();  // Consumir el salto de línea
+
+            switch (opcion) {
+                case 1:
+                    jugarPartido();
+                    break;
+                case 2:
+                    mostrarTablaDePosiciones();
+                    break;
+                case 3:
+                    mostrarCalendario();
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
             }
         }
+    }
 
-        if (equipoUsuario == null) {
-            System.out.println("No se encontró el equipo del usuario en la liga.");
-            return;
-        }
-
-        Random rand = new Random();
-
-        // Jugar partido del equipo del usuario
-        Equipo equipoAleatorio = null;
-        do {
-            equipoAleatorio = equipos.get(rand.nextInt(equipos.size()));
-        } while (equipoAleatorio == equipoUsuario);
-
-        Partido partidoUsuario = new Partido(equipoUsuario, equipoAleatorio);
-        partidoUsuario.jugarPartido();
-
-        // Simular partidos de los otros equipos
-        for (Equipo equipo : equipos) {
-            if (!equipo.nombreEquipo.equals("Equipo del Usuario")) {
-                equipoAleatorio = null;
-                do {
-                    equipoAleatorio = equipos.get(rand.nextInt(equipos.size()));
-                } while (equipoAleatorio == equipo);
-
-                Partido partidoSimulado = new Partido(equipo, equipoAleatorio);
-                partidoSimulado.jugarPartido();
-                System.out.println("Resultado del partido simulado: " + equipo.nombreEquipo + " vs " + equipoAleatorio.nombreEquipo);
-                System.out.println("Goles " + equipo.nombreEquipo + ": " + partidoSimulado.getGolesLocal() +
-                        " - Goles " + equipoAleatorio.nombreEquipo + ": " + partidoSimulado.getGolesVisitante());
+    public void generarCalendario() {
+        for (int i = 0; i < equipos.size(); i++) {
+            for (int j = i + 1; j < equipos.size(); j++) {
+                Equipo equipoLocal = equipos.get(i);
+                Equipo equipoVisitante = equipos.get(j);
+                partidos.add(new Partido(equipoLocal, equipoVisitante, equipoUsuario));
             }
         }
+    }
+
+    public void jugarPartido() {
+        if (partidoActual < partidos.size()) {
+            Partido partido = partidos.get(partidoActual);
+            partido.jugarPartido();
+            partidoActual++;
+        } else {
+            System.out.println("No hay más partidos para jugar.");
+        }
+    }
+
+    public void mostrarTablaDePosiciones() {
+        Collections.sort(equipos, Comparator.comparingInt(Equipo::getPuntos).reversed());
+
+        System.out.println("Tabla de posiciones:");
+        for (Equipo equipo : equipos) {
+            System.out.println(equipo.nombreEquipo + " - Puntos: " + equipo.getPuntos());
+        }
+    }
+    public void mostrarCalendario() {
+        System.out.println("Calendario de Partidos:");
+        for (int i = 0; i < partidos.size(); i++) {
+            Partido partido = partidos.get(i);
+            System.out.println((i + 1) + ". " + partido.equipoLocalPartido.nombreEquipo + " vs " + partido.equipoVisitantePartido.nombreEquipo);
+        }
+    }
+    public boolean verificarAscenso() {
+        Collections.sort(equipos, Comparator.comparingInt(Equipo::getPuntos).reversed());
+        return equipos.get(0).equals(equipoUsuario);
+    }
+
+    public void reiniciarLiga() {
+        for (Equipo equipo : equipos) {
+            equipo.sumarPuntos(-equipo.getPuntos());
+            equipo.setVictorias(0);
+            equipo.setEmpates(0);
+            equipo.setDerrotas(0);
+        }
+        partidos.clear();
+        partidoActual = 0;
+        generarCalendario();
     }
 }
